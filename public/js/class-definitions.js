247145f329039295ab3d94980e4057e72aababfc
Te Vallee <astronomemusic@gmail.com>
2014-10-14 20:13:35 -1000

    88 passed

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -815,7 +815,15 @@ House.prototype.isATallStory = function(storiesTooTall) {
  * Return true if isOn is true, false otherwise.
  *
  */
-
+Lightbulb.prototype.flipSwitch = function(on) {
+  if(on === "on"){
+    this.isOn = true;
+    return true;
+  } else {
+    this.isOn = false;
+    return false;
+  }
+}
 
  /* Step 89
  *
